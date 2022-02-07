ea7e7f5c7866ef0d50f1c4968d2f6047e1f614ed
prashant13031995 <prashantgundeli123@gmail.com>
2022-02-07 14:03:06 +0530

    add a project

---
+++ com.mavenDemo/src/test/java/com/jbk/tests/DemoTest.java
@@ -0,0 +1,35 @@
+package com.jbk.tests;
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.Assert;
+	import org.testng.annotations.Test;
+
+
+
+
+	public class DemoTest 
+	{
+		WebDriver driver;
+		
+		@Test
+		public void test01()
+		{
+			System.setProperty("webdriver.chrome.driver","c:/chromedriver.exe");
+					
+			driver = new ChromeDriver();
+			driver.get("file:///C://Users//HP//ojt//selenium%20ojt//Offline%20Website//Offline%20Website//index.html");
+			driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
+			driver.findElement(By.id("password")).sendKeys("123456");
+			driver.findElement(By.xpath("//button")).click();
+			Assert.assertEquals(driver.getTitle(),"JavaByKiran | Dashboard");
+
+
+		}
+		
+
+
+	}
+
+
+
